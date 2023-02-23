cbe0ca50dfc66a26e8462d9d6faa853fdaad5349
martin <mwirigi.8kabush@gmail.com>
2023-02-23 02:26:01 -0800

    Nature made the natural numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
  *  print_times_table - prints the n times table, starting with 0
- *  @n: number of the times table
  */
 void print_times_table(int n)
 {
 	int i, j, k;
-
 
 	if (n >= 0 && n <= 15)
 	{
