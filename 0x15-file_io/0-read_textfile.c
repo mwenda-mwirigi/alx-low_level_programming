59547fb6b6f7b49c078929d304a8eef45b4565cf
martin-mwenda <mwirigi.8kabush@gmail.com>
2023-05-26 01:52:08 -0700

    three commits

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- function to Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: numb of letters that will be read
+ * Return: w- actual numb of bytes read and printed
+ *        0 when func fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
+i
