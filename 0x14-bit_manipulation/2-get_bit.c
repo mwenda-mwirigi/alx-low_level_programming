a6cdfa6bd0d32ffa3eabe9dde8566edb7cf45079
martin <mwirigi.8kabush@gmail.com>
2023-07-18 20:09:23 -0700

    new file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,5 @@
 i#include "main.h"
 
-
 /**
  * get_bit - Function will return value of a bit at a given index in a dec num
  *
@@ -11,14 +10,13 @@ i#include "main.h"
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value;
+	int bit_val;
 
 	if (index > 63)
 		return (-1);
 
-	bit_value = (n >> index) & 1;
+	bit_val = (n >> index) & 1;
 
-	return (bit_value);
+	return (bit_val);
 }
-
 
